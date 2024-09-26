385d06ff191401507e606ba6399b8465b21597d2
sharianakram <sharianakram0503@gmail.com>
2024-09-26 16:18:24 -0400

    caught up

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -2,6 +2,19 @@ package com.pluralsight;
 
 public class Demo1 {
     public static void main(String[] args){
-        System.out.println("Check!!!");
+        char status = 's';
+        int identifier = 1;
+        String name = "Sharian";
+        int daysOfWeekOfTV = 5;
+        int minPerDayWatched = 80;
+        int minPerWeek = daysOfWeekOfTV * minPerDayWatched;
+        int minPerYear = minPerWeek * 52;
+
+        int hoursPerYear = minPerYear / 60;
+
+        String statement = "Sharian Spends " + hoursPerYear + " wasting time on TV";
+        System.out.println(statement);
+
+
     }
 }
